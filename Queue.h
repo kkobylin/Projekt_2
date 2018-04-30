efcd7965f51b4f3aa17b643f494810cc63717729
kkobylin <37050099+kkobylin@users.noreply.github.com>
2018-04-30 19:13:27 +0200

    Update Queue.h
    Lista dwukierunkowa wykorzystywana w słowniku

--- Queue.h
+++ Queue.h
@@ -5,27 +5,73 @@ Projekt 2 na PROI*/
 #define que_definition
 
 #include <iostream>
-#include "element.h"
 using namespace std;
 
 /* Szablon klasy reprezentujacą kolejkę*/
 
-template <typename key_type, typename type_value>
+template <typename ktype, typename vtype>
 class Que
 {
 public:
+    struct Element
+    {
+        Element *next, *prev;
+        ktype key;
+        vtype value;
+        Element(ktype k,vtype v):key(k), value(v), next(nullptr), prev(nullptr){}
+    };
 
-
-    Element <key_type,type_value> *qfirst, *qlast;
+    Element *qfirst, *qlast;
     int size;
 
 
     Que();  //konstruktor
     ~Que(); //destruktor
     int Size(); //rozmiar listy
-    Element <key_type,type_value>* add_front(key_type, type_value); //dodawanie na poczatek
-    Element <key_type,type_value>* add_back(key_type, type_value); //dodawanie na koniec
-    Element <key_type,type_value>* erase(Element *); //usuwanie danego elementu
+    void add_front(ktype, vtype); //dodawanie na poczatek
+    void add_back(ktype, vtype); //dodawanie na koniec
+    void erase(Element *); //usuwanie danego elementu
 
 };
 #endif
+
+template <typename ktype, typename vtype>
+Que<ktype, vtype>::Que()
+{
+    qfirst = qlast = nullptr;
+    size=0;
+}
+
+template <typename ktype, typename vtype>
+Que<ktype, vtype>::~Que()
+{
+    Element * p;
+    while(qfirst)
+    {
+        p=qfirst->next;
+        delete qfirst;
+        qfirst = p;
+    }
+}
+
+template <typename ktype, typename vtype>
+int Que<ktype, vtype>::Size()
+{
+    return size;
+}
+
+
+template <typename ktype, typename vtype>
+void Que<ktype, vtype>::add_front(ktype k, vtype v)
+{
+    Element* newElement = new Element(k, v); //alokowanie pamieci na nowy element
+    if(this->qfirst == nullptr) this->qfirst = newElement; //Jesli kolejka pusta
+    else
+    {
+        newElement->next = this->qfirst;
+        this->qfirst->prev = newElement;
+        this->qfirst = newElement;
+    }
+}
+
+
