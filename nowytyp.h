394247b7cbc765e85f433f03d712df45b7ba09b0
kkobylin <37050099+kkobylin@users.noreply.github.com>
2018-05-07 20:29:19 +0200

    Update nowytyp.h
    Nowy typ zdefiniowany do testow

--- nowytyp.h
+++ nowytyp.h
@@ -4,16 +4,24 @@
 #include <iostream>
 using namespace std;
 
+//Nowy zdefiniowany typ
+
 class Type
 {
-
+//Zawiera w sobie znak oraz zmienna typu double
 char znak;
 double indeks;
 
 public:
-    Type(char z='a', double i=0);
-    bool operator==(Type const &v);
+    Type(char z='a', double i=0); //konstruktor
+    bool operator==(Type const &v); //Przeciazenie operatora przyrownania
+    void operator=(Type const &v);  //Przeciazenie operatora przypisania
+     bool operator!=(Type const &v);  //Porownywanie wektorow
 
+
+    //Funkcja zaprzyjazniona drukujaca wektor
+    friend
+    ostream& operator<< (ostream &os, const Type &v);
 };
 
 
