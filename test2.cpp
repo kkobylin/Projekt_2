47cbd90e46c781a8f004a231a10477104104a26c
kkobylin <37050099+kkobylin@users.noreply.github.com>
2018-05-07 20:32:14 +0200

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -1,49 +1,44 @@
 #include <iostream>
-#include "test1.h"
 #include "test2.h"
 
 using namespace std;
 
 bool Testit:: testadd  ()
     {
-        dictionary1.add_front(5, ('a',1));
-        dictionary1.add_front(7, ('b',2));
-        dictionary1.add_front(9, ('c',3));
+        Type a('a', 1.1), b('b', 2.2), c('c', 3.3);
+        dictionary1.add_front(5, a);
+        dictionary1.add_front(7, b);
+        dictionary1.add_front(9, c);
 
-        if(dictionary1.findvalue(7)==('b',2))
+        if(dictionary1.findvalue(7)== b)
             return 1;
         else
             return 0;
     }
-/*
-bool Testis:: testprint  ()
+
+void Testit:: testprint  ()
     {
-        dictionary1.add_front(5, "Wydrukowana tresc");
-
-        ostream os;
         dictionary1.print();
-        os<<"5 Wydrukowana tresc";
-
-        if(os==cout)
-            return 1;
-        else
-            return 0;
+        dictionary2.print();
     }
-*/
+
 
 bool Testit:: testdelete()
 {
-    dictionary1.delet(7, ('b',2));
-    if(dictionary1.Size()==2)
+
+   Type b('b',2.2);
+    dictionary1.delet(7,b);
+    if(dictionary1.Size()==3)
         return 1;
     else
         return 0;
 }
-/*
+
 bool Testit:: testchange()
 {
-    dictionary1.change(5, "Czwarty");
-    if(dictionary1.findvalue(5)=="Czwarty")
+    Type h('h', 0);
+    dictionary1.change(5, h);
+    if(dictionary1.findvalue(5)==h)
         return 1;
     else
         return 0;
@@ -51,7 +46,8 @@ bool Testit:: testchange()
 
 bool Testit:: testfind()
 {
-    if(dictionary1.findvalue(9)=="Trzeci")
+    Type c('c', 3.3);
+    if(dictionary1.findvalue(9)==c)
         return 1;
     else
         return 0;
@@ -59,7 +55,8 @@ bool Testit:: testfind()
 
 bool Testit:: testhowmany()
 {
-    dictionary1.add_front(5, "Piaty");
+    Type d('e', 2.12);
+    dictionary1.add_front(5,d);
     if(dictionary1.howmany(5)==2)
         return 1;
     else
@@ -68,24 +65,27 @@ bool Testit:: testhowmany()
 
 bool Testit:: testsize()
 {
-    if(dictionary1.Size()==3)
+    if(dictionary1.Size()==4)
         return 1;
     else
         return 0;
 }
-/*
+
 bool Testit:: testcom()
 {
-    dictionary2.add_front(5, "Czwarty");
-    dictionary2.add_front(9, "Trzeci");
-    dictionary2.add_front(5, "Piaty");
-    bool a;
-    a=comparison(dictionary1, dictionary2);
-   if(a==1)
-    return 1;
-   else
-    return 0;
+    Type a('h', 0), b('b', 2.2), c('c', 3.3), d('e', 2.12);
 
-}*/
+    dictionary2.add_front(5, a);
+    dictionary2.add_front(7, b);
+    dictionary2.add_front(9, c);
+    dictionary2.add_front(5, d);
 
+    return comparison(dictionary1, dictionary2);
 
+}
+
+void Testit:: cleaning()
+{
+    dictionary1.~Que();
+    dictionary2.~Que();
+}
