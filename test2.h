1fc85f6865b3a86e15a8438a86afc109e3b0d425
kkobylin <37050099+kkobylin@users.noreply.github.com>
2018-05-07 20:31:10 +0200

    Update test2.h
    Klasa testujaca multislownik dla typow int oraz nowego

--- test2.h
+++ test2.h
@@ -1,16 +1,15 @@
 /*Krzysztof Kobyliñski
 Projekt 2 na PROI*/
 
-#ifndef multidictionarytestintvector_definition
-#define multidictionarytestintvector_definition
+#ifndef multidictionarytestintType_definition
+#define multidictionarytestintType_definition
 
 #include <iostream>
 #include "Queue.h"
-#include "vector.h"
 #include "nowytyp.h"
 using namespace std;
 
-/*Szablon Klasy testującej klasę Multisłownika <int, string>*/
+/*Szablon Klasy testującej klasę Multisłownika <typ wbudowany, nowy zdefiniowany typ>*/
 
 class Testit
 {
@@ -19,13 +18,13 @@ class Testit
 
     public:
         bool testadd  (); //metoda testująca dodawanie elementów
-        bool testprint (); //metoda testująca drukowanie słownika
+        void testprint (); //metoda testująca drukowanie słownika
         bool testdelete(); // metoda testująca usuwanie elemntów
         bool testchange (); //metoda testująca podmianę wartości
         bool testfind (); // metoda testująca wyszukiwanie wartości
         bool testhowmany (); //metoda testujaca zliczanie elementow o podanym kluczu
         bool testsize (); //metoda testujaca podawanie rozmiaru slownika
-       // bool testcom(); //metoda testujaca porownywanie slownikow
+        bool testcom(); //metoda testujaca porownywanie slownikow
         void cleaning(); //metoda czyszczaca po ostatnim tescie
 
 };
